a417ad30597f1bf61e05a1724cb4eab3f963af03
fallow64 <fallow640@gmail.com>
2025-12-05 16:00:56 -0600

    Modify homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,19 @@ import Grid from "@mui/material/Grid";
 export default async function Home() {
   return (
     <Box sx={{ minHeight: "100vh", bgcolor: "background.default" }}>
-      <Container maxWidth="lg" sx={{ my: 2 }}>
+      <Container maxWidth="lg" sx={{ my: 4 }}>
         <Typography variant="h4" component="h2" gutterBottom>
           CHTC User App Beta
+        </Typography>
+
+        <Typography variant="body1" gutterBottom>
+          Test credentials (with mock data):
+          <Box component="div">
+            Username: <strong>admin</strong>
+          </Box>
+          <Box component="div">
+            Password: <strong>password</strong>
+          </Box>
         </Typography>
       </Container>
     </Box>
